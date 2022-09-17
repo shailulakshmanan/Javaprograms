e38e7c9db21e120abaee8e7ea0642d819f68e987
shailulakshmanan <113494079+shailulakshmanan@users.noreply.github.com>
2022-09-17 13:15:20 +0530

    Add files via upload

--- StudentDetails.java
+++ StudentDetails.java
@@ -7,6 +7,8 @@ class Student{
 	int studentage;
 	float studentfees;
 	String studentemail;
+	float total,average;
+	int eng,math,sci,soci;
 
 
 void studentInput() {
@@ -20,6 +22,22 @@ void studentInput() {
 	System.out.println("Enter email");
 	studentemail=sc.next();
 	}
+void inputMarks() {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Student Marks: English,maths,science,social");
+	eng=sc.nextInt();
+	math=sc.nextInt();
+	sci=sc.nextInt();
+	soci=sc.nextInt();
+}
+void calculateTotalAverage() {
+	total=eng+math+sci+soci;
+	average=total/4;
+	System.out.println("total="+total);
+	System.out.println("average="+average);
+}
+
+
 
 void StudentDisplay() {
 	System.out.println("Name="+studentname);
@@ -28,9 +46,9 @@ void StudentDisplay() {
 	System.out.println("Email="+studentemail);
 	
 }
-
-	
 }
+	
+
 
 public class StudentDetails {
 	public static void main(String[] args) {
@@ -38,5 +56,7 @@ public class StudentDetails {
 	Student studentobject=new Student();
 	studentobject.studentInput();
 	studentobject.StudentDisplay();
+	studentobject.inputMarks();
+	studentobject.calculateTotalAverage();
 }
 }
