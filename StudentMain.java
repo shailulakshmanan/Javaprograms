60f27f39d15a13c25ec98f8deb607058e5b6b6d1
shailulakshmanan <113494079+shailulakshmanan@users.noreply.github.com>
2022-11-28 21:53:57 +0530

    Add files via upload

--- StudentMain.java
+++ StudentMain.java
@@ -1,28 +1,43 @@
-package com.inter;
+package com.edub;
+		     
+import java.util.Iterator;
+import java.util.List;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
-public class StudentMain {
+public class StudentMain{
 
-	public static void main(String[] args) throws NumberFormatException, IOException{
-		int sid;
-		String sname;
-		
-		InputStreamReader is=new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(is);
-		
-		System.out.println("Enter studentid");
-		sid=Integer.parseInt( br.readLine());
-		System.out.println("Enter name of the student");
-		sname=br.readLine();
-		
-		System.out.println("id="+sid);
-		System.out.println("name ="+sname);
-		
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Query query = em.createQuery("delete from Studentjpa where id=1");
+		query.executeUpdate();
+		System.out.println("Record Deleted");
+		Query querySelect = em.createQuery("Select s from Studentjpa s");
+
+		@SuppressWarnings("unchecked")
+		List<StudentJPA> list = (List<StudentJPA>) querySelect.getResultList();
+
+		Iterator<StudentJPA> it = list.iterator();
+		System.out.println("ID\t  Name\t\tFees");
+		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+		while (it.hasNext()) {
+			StudentJPA sob = it.next();
+			// System.out.println(sob.getId() + "\t" + sob.getName() + "\t" +
+			//System.out.println(sob.getId()+"\t"+sob.getName()+"\t"+sob.getFees());
+        	System.out.printf("%d",sob.getId());
+        	System.out.printf("%15s",sob.getName());
+        	System.out.printf("\t%f",sob.getFees());
+        	System.out.printf("\n");//System.out.println();
+        	
+        }
+
+		em.getTransaction().commit();
 
 	}
+
 }
-
-
