d352f45177b8c2e4f4643cf4c1f022baf7fcc65f
shailulakshmanan <113494079+shailulakshmanan@users.noreply.github.com>
2022-11-28 22:04:19 +0530

    Add files via upload

--- StudentMain.java
+++ StudentMain.java
@@ -1,11 +1,14 @@
 package com.edub;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+
+import org.hibernate.internal.build.AllowSysOut;
+import org.hibernate.query.Query;
 
 public class StudentMain {
 
@@ -13,39 +16,35 @@ public class StudentMain {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
 		EntityManager em = emf.createEntityManager();
 		
-
-		em.getTransaction().begin();	
 		
-		  Query query = em.createQuery( "update StudentJPA SET fees=8500 where id=2");  
-         query.executeUpdate();  
-
-       em.getTransaction().commit();
-
-       
-	    Query query1 = em.createQuery( "Select s from StudentJPA s");  
-       
-       @SuppressWarnings("unchecked")  
-     List<StudentJPA> list=(List<StudentJPA>)query1.getResultList( );  
-        
-       System.out.print("ID");  
-       System.out.print("\t Name");  
-      System.out.println("\tFees");  
-         
-         
-       for( StudentJPA s:list ){  
-          System.out.print( s.getId( ));  
-          System.out.print("\t" +  s.getName( ));  
-          System.out.print("\t" + s.getFees( ));  
-          System.out.println();  
-          }  
-       em.getTransaction().commit();  
-         
-
-		
-			
-		
-		
-		}
-	}
-
+		em.getTransaction().begin( );  
+		        
+			    Query query = (Query) em.createQuery( "Select s from StudentJPA s");  
+		        
+		        @SuppressWarnings("unchecked")  
+		      List<StudentJPA> list=(List<StudentJPA>)query.getResultList( );  
+		         
+		       /* System.out.print("ID");  
+		        System.out.print("\t Name");  
+		       System.out.println("\tFees");  
+		          
+		          
+		        for( StudentJPA s:list ){  
+		           System.out.print( s.getId( ));  
+		           System.out.print("\t" +  s.getName( ));  
+		           System.out.print("\t" + s.getFees( ));  
+		           System.out.println();  
+		           }  
+		        em.getTransaction().commit();  
+		          
+		     }*/
+		//try with iterator
+		        Iterator<StudentJPA> it=list.iterator();
+		        System.out.println("ID\tName\tFees");
+		        while(it.hasNext()) {
+		        	StudentJPA sob=it.next();
+		        	System.out.println(sob.getId()+"\t"+sob.getName()+"\t"+sob.getFees());
+		        }
+}
+}
 
