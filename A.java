1db64f88d380c035a372e10b685071464fb3ad48
shailulakshmanan <113494079+shailulakshmanan@users.noreply.github.com>
2022-11-28 21:57:35 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,13 +1,16 @@
-package com.oppo;
+package com.fact;
 
-public class A implements Printable{
-
-	public void print() {
-		System.out.println("Hello A");
-		
-	}  
-     
-  
+public class A {  
+private static final A obj=new A();  
+private A()
+{
+	System.out.println("private constructor");
 }  
-
-
+public static A getA(){  
+    System.out.println("factory method ");  
+    return obj;  
+}  
+public void msg(){  
+    System.out.println("hello user");  
+}  
+}  
--- Test.java
+++ Test.java
@@ -1,12 +1,16 @@
-package com.oppo;
-
+package com.fact;
 
 import org.springframework.context.ApplicationContext;  
 import org.springframework.context.support.ClassPathXmlApplicationContext;  
 public class Test {  
 public static void main(String[] args) {  
     ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");  
-    Printable p=(Printable)context.getBean("p");  
-    p.print();  
+    A a=(A)context.getBean("a");  
+    a.msg();  
+    A a1=(A)context.getBean("a1");  
+    a1.msg();
 }  
 }  
+	
+
+
