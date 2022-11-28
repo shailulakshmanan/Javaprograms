172174582fb82ee04190ba2919f7f696711d788c
shailulakshmanan <113494079+shailulakshmanan@users.noreply.github.com>
2022-11-28 21:58:16 +0530

    Add files via upload

--- StudentMain.java
+++ StudentMain.java
@@ -1,14 +1,14 @@
 package com.edub;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
-
-
+import org.hibernate.internal.build.AllowSysOut;
+import org.hibernate.query.Query;
 
 public class StudentMain {
 
@@ -18,12 +18,30 @@ public class StudentMain {
 		
 		em.getTransaction().begin();	
 		
-	
-Query q=em.createQuery("delete from StudentJPA where id=1");
-q.executeUpdate();
-em.getTransaction().commit();
+		Query query = (Query) em.createQuery("select name from StudentJPA");
 		
+		List<String> list = query.getResultList();
+		System.out.println("Student Name:");
+		for(String s:list) {
+			System.out.println(s);
+			
+
+	        Iterator<StudentJPA> it=list.iterator();
+            System.out.println("ID\t  Name\t\tFees");
+	        System.out.println("-----------------------------------------------");
+	        
+	        while(it.hasNext()) {
+	        	StudentJPA sob=it.next();
+	        	//System.out.println(sob.getId()+"\t"+sob.getName()+"\t"+sob.getFees());
+	        	System.out.printf("%d",sob.getId());
+	        	System.out.printf("%15s",sob.getName());
+	        	System.out.printf("\t%f",sob.getFees());
+	        	System.out.printf("\n");//System.out.println();
+	        	
+	        }
+	        em.getTransaction().commit();
 		}
 	}
+}
 
 
